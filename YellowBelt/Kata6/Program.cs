5f34e717ec8d8e7226e05fbcb26ee878ee2b150c
Dev-Luteus <EmilDev.Luteus@gmail.com>
2024-11-07 14:49:31 +0100

    Kata6

---
+++ YellowBelt/Kata6/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Kata6;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] enemies = {"Goblin","Orc","Troll","Skeleton","Dragon"};
+        List<string> playerInventory = new List<string>(){"Sword", "Shield", "Potion"};
+        
+        Console.WriteLine("Enemies:");
+        foreach (var enemyName in enemies)
+        {
+            Console.WriteLine(enemyName);
+        }
+        
+        Console.WriteLine("\nPlayer inventory:");
+        PrintList(playerInventory);
+        
+        playerInventory.Add("Armor");
+        playerInventory.Add("Helmet");
+        playerInventory.Remove("Potion");
+        
+        Console.WriteLine("\nUpdated Player Inventory");
+        PrintList(playerInventory);
+        Console.WriteLine($"\nTotal items in Player Inventory: {playerInventory.Count}");
+    }
+    static void PrintList(List<string> list)
+    {
+        foreach (var itemName in list)
+        {
+            Console.WriteLine(itemName);
+        }
+    }
+}
---
+++ YellowBelt/MiniKata7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Threading.Channels;
+
+namespace MiniKata7;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var User = new Player();
+        
+        User.name = "Arin";
+        User.health = 100;
+        User.level = 1;
+        
+        Console.WriteLine($"{User.name}" + $"\n{User.health}" + $"\n{User.level}");
+        var Goblin = new Enemy("Goblin", 50, 10);
+    }
+}
+
+class Player()
+{
+    public string name { get; set; }
+    public int health { get; set; }
+    public int level { get; set; }
+}
+class Enemy(string type, int health, int damage)
+{
+}
