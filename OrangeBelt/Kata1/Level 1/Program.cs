a3b221af5ba733da8aad06844a7f9ddeea1bdac0
Dev-Luteus <EmilDev.Luteus@gmail.com>
2024-11-18 13:25:46 +0100

    Striped Yellow

--- OrangeBelt/Kata1/Level 1/Program.cs
+++ OrangeBelt/Kata1/Level 1/Program.cs
@@ -1,19 +1,58 @@
-﻿namespace Level_1;
-
-class Program
+﻿namespace Level_1 
 {
-    static void Main(string[] args)
+    class Program 
     {
-        
+        static void Main(string[] args) 
+        {
+            var characters = new List<dynamic> 
+            { 
+                new Warrior("Abel", 80, () => Console.WriteLine($"Abel Attacks!")),
+                new Warrior("Babel", 100, () => Console.WriteLine($"Babel Attacks!")),
+                new Warrior("Zara", 40, () => Console.WriteLine($"Zara Attacks!")),
+                new Healer("Eva", 50, chars => 
+                {
+                    var lowestWarrior = chars
+                        .OrderBy(c => c.Health)
+                        .First(c => c is Warrior);
+                    Console.WriteLine($"Eva heals {lowestWarrior.Name}!");
+                })
+            };
+
+            characters = characters.OrderBy(c => c.Health).ToList();
+
+            characters.ForEach(character => 
+            {
+                Console.WriteLine($"{character.Name} | Health: {character.Health}");
+                character.PrimaryAction(characters);
+            });
+        }
+
+        class Warrior 
+        {
+            public string Name { get; set; }
+            public int Health { get; set; }
+            public Action<List<dynamic>> PrimaryAction { get; }
+
+            public Warrior(string name, int health, Action primaryAction) 
+            {
+                Name = name;
+                Health = health;
+                PrimaryAction = _ => primaryAction();
+            }
+        }
+
+        class Healer 
+        {
+            public string Name { get; set; }
+            public int Health { get; set; }
+            public Action<List<dynamic>> PrimaryAction { get; }
+
+            public Healer(string name, int health, Action<List<dynamic>> primaryAction) 
+            {
+                Name = name;
+                Health = health;
+                PrimaryAction = primaryAction;
+            }
+        }
     }
-}
-
-class Warrior
-{
-    
-}
-
-class Healer
-{
-    
 }
