d6e2dd519b5f5cb711b2009f64f3cd4b3070a970
Dev-Luteus <EmilDev.Luteus@gmail.com>
2024-11-19 15:35:09 +0100

    fix

---
+++ OrangeBelt/Kata2/Character.cs
@@ -0,0 +1,21 @@
+﻿namespace Kata2;
+
+public class Character
+{
+    private Action Action { get; set; }
+    public event Action<int> HealthChanged;
+    public string Name { get; set; }
+    public int Health { get; set; }
+
+    public void Attack(Character target, int damage)
+    {
+        Action?.Invoke();
+        target.Health -= damage;
+        HealthChanged?.Invoke(target.Health);
+    }
+    public Character(string name, int health)
+    {
+        Name = name;
+        Health = health;
+    }
+}
--- OrangeBelt/Kata2/Program.cs
+++ OrangeBelt/Kata2/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var charEdgard = new Character("Edgard", 100);
+        var charYourEx = new Character("Your Ex", 100);
+
+        Console.WriteLine($"{charEdgard.Name} is attacking {charYourEx.Name}!");
+        Console.WriteLine($"{charYourEx.Name} Max Health: {charYourEx.Health}");
+        
+        charEdgard.Attack(charYourEx, 20);
+        charYourEx.Health -= charYourEx.HealthChanged.Invoke();
     }
 }
